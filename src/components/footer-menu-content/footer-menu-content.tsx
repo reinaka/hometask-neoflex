bf6dfac909f8a9cebc13cb972d2533ac5cc61620
reinaka <juliabednaia@gmail.com>
2024-09-23 06:02:44 +0300

    changed footer links styles

--- src/components/footer-menu-content/footer-menu-content.tsx
+++ src/components/footer-menu-content/footer-menu-content.tsx
@@ -24,7 +24,7 @@ export const FooterMenuContent = ({ className, setMobileMenuOpen } : {
         <nav className={`${styles.navigationLayout} ${className || ''}`}>
             <ul className={styles.navigation}>
                 {menuItems.map((item) => (
-                    <li onClick={setMobileMenuOpen ? () => setMobileMenuOpen(false) : undefined}>
+                    <li onClick={setMobileMenuOpen ? () => setMobileMenuOpen(false) : undefined} key={item.title}>
                         <NavLink to={item.link}>
                             {item.title}
                         </NavLink>
@@ -33,7 +33,13 @@ export const FooterMenuContent = ({ className, setMobileMenuOpen } : {
             </ul>
 
             <div className={styles.navigation}>
-                <a href="https://www.neoflex.ru/%D0%9F%D0%BE%D0%BB%D0%B8%D1%82%D0%B8%D0%BA%D0%B0%20%D0%BE%D0%B1%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D0%BA%D0%B8%20%D0%BF%D0%B5%D1%80%D1%81%D0%BE%D0%BD%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D1%85%20%D0%B4%D0%B0%D0%BD%D0%BD%D1%8B%D1%85.pdf" target="_blank" rel="noreferrer">Условия сервиса</a>
+                <a 
+                    href="https://www.neoflex.ru/%D0%9F%D0%BE%D0%BB%D0%B8%D1%82%D0%B8%D0%BA%D0%B0%20%D0%BE%D0%B1%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D0%BA%D0%B8%20%D0%BF%D0%B5%D1%80%D1%81%D0%BE%D0%BD%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D1%85%20%D0%B4%D0%B0%D0%BD%D0%BD%D1%8B%D1%85.pdf"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    <span>Условия сервиса</span>
+                </a>
                 <div className={styles.additionalNavigation}>
                     <img src={icon} alt="language icon" className={styles.icon}/>
                     <div>
